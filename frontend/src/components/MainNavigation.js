3b3beb31e3a35a9f50b107f93d591ba6a7a3b160
ivanhb <ivanxhb@gmail.com>
2023-07-05 22:15:42 +0000

    some changes

---
+++ frontend/src/components/MainNavigation.js
@@ -0,0 +1,25 @@
+import './MainNavigation.css';
+
+function MainNavigation() {
+
+    return (
+
+
+
+      <div class="MainNavigation">
+        <nav>
+        <a class="mainNavLink" href="/html/">HTML</a> |
+        <a class="mainNavLink" href="/css/">CSS</a> |
+        <a class="mainNavLink" href="/js/">JavaScript</a> |
+        <a class="mainNavLink" href="/python/">Python</a>
+        </nav>
+      </div>
+    );
+  
+   
+
+  }
+  
+
+
+  export default MainNavigation;
